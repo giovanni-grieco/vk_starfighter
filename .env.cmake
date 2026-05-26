5fc827ca7f510c8d89676b1ce6ed7e0dab392029
giovanni <giovi.5057@gmail.com>
2026-05-26 10:45:19 +0200

    works now!

--- .env.cmake
+++ .env.cmake
@@ -1,2 +1,4 @@
 # Optional set TINYOBJ_PATH to target specific version, otherwise defaults to external/tinyobjloader
 # set(TINYOBJ_PATH /Users/brendan/dev/tinyobjloader)
+
+set(GLSL_VALIDATOR glslc)
